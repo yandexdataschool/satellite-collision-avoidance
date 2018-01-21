7aae347c93f3348a21f8e37c111b5c6b31a5abe4
Dmitry <dubiman2011@gmail.com>
2018-01-21 10:55:17 +0300

    add start_time as argument

--- collision.py
+++ collision.py
@@ -17,21 +17,23 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--visualize", type=str,
                         default="True", required=False)
-    parser.add_argument("-t", "--end_time", type=float,
+    parser.add_argument("-start", "--start_time", type=float,
+                        default=START_TIME, required=False)
+    parser.add_argument("-end", "--end_time", type=float,
                         default=END_TIME, required=False)
     parser.add_argument("-s", "--step", type=float,
                         default=SIMULATION_STEP, required=False)
     args = parser.parse_args(args)
 
     visualize = args.visualize.lower() == "true"
-    end_time, step = args.end_time, args.step
+    start_time, end_time, step = args.start_time, args.end_time, args.step
 
     osc = read_space_objects("data/collision.osc", "osc")
     protected = osc[0]
     debris = [osc[1]]
 
     agent = Agent()
-    start_time = pk.epoch(START_TIME, "mjd2000")
+    start_time = pk.epoch(start_time, "mjd2000")
     env = Environment(protected, debris, start_time)
 
     simulator = Simulator(agent, env, print_out=False)
