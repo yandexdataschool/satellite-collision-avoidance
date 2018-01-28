78a558ba57f4611ce2345d225b688bfa3b1e8389
Dmitry <dubiman2011@gmail.com>
2018-01-28 19:17:53 +0300

    Update act test

--- test_api.py
+++ test_api.py
@@ -1,6 +1,16 @@
 # We need to test basic funcitons, Environment, SpaceObject
 import unittest
 
+import pykep as pk
+import numpy as np
+
+from api import Environment, SpaceObject
+
+from api import fuel_consumption
+
+# will be used in maneuver testing.
+from api import MAX_PROPAGATION_STEP, MAX_FUEL_CONSUMPTION
+
 
 class TestBasicFunctions(unittest.TestCase):
 
@@ -31,8 +41,34 @@ class TestEnvironment(unittest.TestCase):
     def test_update_collision_probability(self):
         self.assertTrue(True)
 
-    def test_act(self):
-        self.assertTrue(True)
+    def test_act_normal(self):
+        start_time = pk.epoch(1, "mjd2000")
+        osculating_elements = (7800, 0.001, 1, 0, 0, 0)
+        mu_central_body, mu_self, radius, safe_radius = 0.1, 0.1, 0.1, 0.1
+        fuel = 10
+        params = dict(elements=osculating_elements, epoch=start_time,
+                      mu_central_body=mu_central_body,
+                      mu_self=mu_self,
+                      radius=radius,
+                      safe_radius=safe_radius,
+                      fuel=fuel)
+        protected = SpaceObject("protected", "osc", params)
+        env = Environment(protected, [], start_time)
+
+        action = np.array([1, 1, 1, 2, 2])
+        dV = action[:3]
+        epoch = pk.epoch(float(action[3]), "mjd2000")
+        # time_to_req = pk.epoch(float(action[4]), "mjd2000")
+
+        prev_fuel = env.protected.get_fuel()
+        fuel_cons = fuel_consumption(dV)
+        env.act(action)
+
+        new_osculating_elements = (0.03322700875329275, 186259.18646581616, 0.7798493139874857,
+                                   6.270834870874244, 5.67768891107007, -137721.8773532762)
+        self.assertEqual(new_osculating_elements,
+                         env.protected.osculating_elements(epoch))
+        self.assertEqual(env.protected.get_fuel(), prev_fuel - fuel_cons)
 
 
 class TestSpaceObject(unittest.TestCase):
@@ -41,9 +77,6 @@ class TestSpaceObject(unittest.TestCase):
         self.assertTrue(True)
 
     def test_position(self):
-        self.assertTrue(True)
-
-    def test_(self):
         self.assertTrue(True)
 
 
