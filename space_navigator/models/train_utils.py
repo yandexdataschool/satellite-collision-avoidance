8302a2c52dc882147995fb430705eb8f8157d3ab
Leonid Gremyachikh <ligremyachikh@edu.hse.ru>
2018-08-08 14:32:24 +0300

    add collision_data function

--- space_navigator/models/train_utils.py
+++ space_navigator/models/train_utils.py
@@ -16,6 +16,7 @@ def generate_session_with_env(agent, env, step):
     Args:
         agent (Agent): agent to do actions.
         env (Environment): environment to simulate session with.
+        step (float): time step in simulation.
 
     Returns:
         reward (float): reward after end of simulation.
@@ -52,8 +53,6 @@ def position_after_actions(action_table, env, step, epoch):
     pos, vel = env.protected.position(epoch)
     env.reset()
     return pos, vel
-
-# TODO - delete generate_session
 
 
 def generate_session(protected, debris, agent, start_time, end_time, step, return_env=False):
@@ -158,15 +157,12 @@ def print_end_train(reward, train_time, action_table):
 def time_to_first_collision_in_env(env, step):
     # TODO - test
     agent = TableAgent()
-    simulator = Simulator(agent, env, step)
-    reward = simulator.run(log=False)
-    collision_moments = env.get_collision_data()
-    if collision_moments:
-        time = collision_moments[0]['epoch'] - env.get_start_time()
+    collisions = collision_data(env, step, agent)
+    if collisions:
+        time = collisions[0]['epoch'] - env.get_start_time().mjd2000
     else:
         # TODO - if None - empty action table for all models
         time = None
-    env.reset()
     return time
 
 
@@ -192,3 +188,16 @@ def projection(plane, vector):
     proj = np.linalg.inv(A.T.dot(A))
     proj = A.dot(proj).dot(A.T).dot(x)
     return proj
+
+
+def collision_data(env, step, agent):
+    simulator = Simulator(agent, env, step)
+    simulator.run(log=False)
+    collision_data = env.get_collision_data()
+    env.reset()
+    return collision_data
+
+
+def change_orbit():
+    """Returns maneuvers to change orbit."""
+    pass
