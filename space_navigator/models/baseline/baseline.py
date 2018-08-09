fbc98595e76bc7d473bef6dc14345cb700214c6c
Leonid Gremyachikh <ligremyachikh@edu.hse.ru>
2018-08-09 18:03:16 +0300

    maneuvers direction and debug

--- space_navigator/models/baseline/baseline.py
+++ space_navigator/models/baseline/baseline.py
@@ -21,7 +21,7 @@ from ..train_utils import (
 class Baseline(BaseTableModel):
     """Provides prograde/retrograde maneuvers collision-by-collision."""
 
-    def __init__(self, env, step, reverse=True):
+    def __init__(self, env, step, reverse=True, first_maneuver_direction="auto"):
         """
         Agrs:
             env (Environment): environment with given parameteres.
@@ -30,6 +30,11 @@ class Baseline(BaseTableModel):
                 if True: there are selected exactly 2 maneuvers
                     while the second of them is reversed to the first one;
                 if False: one maneuver.
+            first_maneuver_direction (str): first maneuver is collinear
+                to the velocity vector and could be:
+                    "forward" (co-directed)
+                    "backward" (oppositely directed)
+                    "auto" (just collinear).
 
         TODO:
             tests - compare with CollinearGridSearch
@@ -40,9 +45,10 @@ class Baseline(BaseTableModel):
 
         self.start_time = self.env.get_start_time()
         self.end_time = self.env.get_end_time()
-
-        self.first_action = np.array(
-            [0, 0, 0, self.time_to_first_maneuver])
+        self.min_time_to_next_maneuver = 0.01  # (days)  # TODO: check
+        self.first_maneuver_direction = first_maneuver_direction
+        self.maneuvers_direction = first_maneuver_direction
+        self._avoided_collisions = []
 
     def iteration(self, print_out=False, n_sessions=100):
         """Training iteration.
@@ -62,10 +68,20 @@ class Baseline(BaseTableModel):
         start_time = copy(self.start_time)
 
         while True:
+            # print("----", "n coll", len(collisions))
             if not collisions:
                 break
 
-            # next collision info
+            # print collision info
+            if print_out:
+                print(f"\nCurrent time: {start_time.mjd2000}")
+                print(f"Collision with: {collisions[0]['debris_name']}.")
+                print("Collision information before maneuver:")
+                print(f"    Time: {collisions[0]['epoch']} (mjd2000);")
+                print(f"    Distance: {collisions[0]['distance']} (meters);")
+                print(f"    Probability: {collisions[0]['probability']}.")
+
+            # next collision info before maneuver
             epsilon = 0.01
             next_collision_epoch = pk.epoch(
                 collisions[0]['epoch'] + epsilon, "mjd2000")
@@ -78,23 +94,25 @@ class Baseline(BaseTableModel):
             )
 
             # Collinear Grid Search model for narrowed environment
+            if print_out:
+                print("Training:")
             model_GS = CollinearGridSearch(
-                narrowed_env, self.step, reverse=False)  # TODO: reverse=reverse
+                narrowed_env, self.step, False, self.maneuvers_direction)
             model_GS.train(1, False, n_sessions)
             action_table = model_GS.action_table
 
-            # update temporary environment
-            t_man = start_time.mjd2000 + model_GS.time_to_first_maneuver
-            t_man = pk.epoch(t_man, "mjd2000")
-            start_time = t_man.mjd2000 + self.step
-            start_time = pk.epoch(start_time, "mjd2000")
-            new_env = Environment(
-                protected=copy(env.protected),
-                debris=copy(env.debris),
-                start_time=start_time,
-                end_time=self.end_time,
-            )
+            # update environment
             if len(action_table) > 0:
+                t_man = start_time.mjd2000 + model_GS.time_to_first_maneuver
+                t_man = pk.epoch(t_man, "mjd2000")
+                start_time = t_man.mjd2000 + self.min_time_to_next_maneuver
+                start_time = pk.epoch(start_time, "mjd2000")
+                new_env = Environment(
+                    protected=copy(env.protected),
+                    debris=copy(env.debris),
+                    start_time=start_time,
+                    end_time=self.end_time,
+                )
                 # change trajectory of protected object
                 # according to obtained maneuver
                 new_env.protected.maneuver(action_table[1, :3], t_man)
@@ -102,32 +120,53 @@ class Baseline(BaseTableModel):
                 action_table[-1, 3] = self.step
                 self.action_table = np.vstack(
                     (self.action_table, action_table))
-            env = new_env
+                env = new_env
 
-            # print iteration info
+            # update maneuvers direction
+            if self.maneuvers_direction == 'auto' and len(action_table) > 0:
+                _, V = env.protected.position(
+                    start_time.mjd2000 + self.action_table[0, 3])
+                is_forward = np.sum(action_table[1, :3] / V) >= 0
+                self.maneuvers_direction = 'forward' if is_forward else 'backward'
+
+            # new collisions info after maneuver
+            if len(action_table) > 0:
+                agent = Agent()
+                new_collisions = collision_data(env, self.step, agent)
+                if new_collisions:
+                    # skip avoided collisions
+                    # TODO: optimize skipping
+                    skip_list = []
+                    for i, new_coll in enumerate(new_collisions):
+                        for avoided_coll in self._avoided_collisions:
+                            # check debris id
+                            if new_coll[0]['debris_id'] == avoided_coll[0]['debris_id']:
+                                # check if it is not
+                                # another turn around the Earth
+                                atol = 0.01  # (days) 0.01 days == 14.4 minutes
+                                if np.isclose(
+                                    new_collisions[0]['epoch'],
+                                    collisions[0]['epoch'],
+                                    atol=atol,
+                                ):
+                                    skip_list.append(i)
+                    new_collisions = list(np.delete(new_collisions, skip_list))
+            else:
+                # skip collision if it is better without maneuver
+                self._avoided_collisions.append(collisions[0])
+                new_collisions = collisions[1:] or []
+
+            # print collision info after maneuver
             if print_out:
-                print(f"Current epoch: {start_time.mjd2000}")
-                print("Collision information:")
-                print(f"    Debris name: {collisions[0]['debris_name']};")
-                print(f"    Collision epoch: {collisions[0]['epoch']} (mjd2000);")
-                print(f"    Distance: {collisions[0]['distance']} (meters);")
-                print(f"    Probability: {collisions[0]['probability']}.")
+                if len(action_table) > 0:
+                    maneuver = action_table[-1]
+                    maneuver[3] = self.start_time.mjd2000 + np.sum(
+                        self.action_table[:-1, 3])
+                    print(f"Maneuver: {maneuver}")
+                else:
+                    print("Maneuver: no maneuvers.")
 
-            # update information about collisions
-            agent = Agent()
-            new_collisions = collision_data(env, self.step, agent)
-            if new_collisions:
-                # check if collision with last piece of debris
-                # is still possible
-                if new_collisions[0]['debris_id'] == new_collisions[0]['debris_id']:
-                    # check if it is not another turn around the Earth
-                    atol = 0.01  # (days) 0.01 days == 14.4 minutes
-                    if np.isclose(
-                            new_collisions[0]['epoch'],
-                            collisions[0]['epoch'],
-                            atol=atol,
-                    ):
-                        new_collisions = new_collisions[1:] or []
+            # update collisions information
             collisions = new_collisions
 
         if self.reverse:
