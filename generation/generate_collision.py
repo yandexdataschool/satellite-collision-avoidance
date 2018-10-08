81caea6dbe2f1cd577f448be4db0c4aee1eec90b
Leonid Gremyachikh <1123qqweaasdzzzxc@gmail.com>
2018-10-08 16:50:10 +0300

    add time_before_start_time

--- generation/generate_collision.py
+++ generation/generate_collision.py
@@ -16,6 +16,8 @@ def main(args):
                         default=6600, required=False)
     parser.add_argument("-end", "--end_time", type=float,
                         default=6600.1, required=False)
+    parser.add_argument("-before", "--time_before_start_time", type=float,
+                        default=0, required=False)
 
     # debris parameters
     parser.add_argument("-p_s", "--pos_sigma", type=float,
@@ -33,6 +35,7 @@ def main(args):
     args = parser.parse_args(args)
 
     n_debris, start_time, end_time = args.n_debris, args.start_time, args.end_time
+    time_before_start_time = args.time_before_start_time
 
     pos_sigma, vel_ratio_sigma = args.pos_sigma, args.vel_ratio_sigma
     i_threshold = args.i_threshold
@@ -46,7 +49,7 @@ def main(args):
     for _ in range(n_debris):
         generator.add_debris(pos_sigma, vel_ratio_sigma, i_threshold)
 
-    generator.save_env(save_path)
+    generator.save_env(save_path, time_before_start_time)
 
     return
 
